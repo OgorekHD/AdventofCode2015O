20c92b2e44ee9297b45bc6c703001d092bc39f8f
OgorekHD <96737093+OgorekHD@users.noreply.github.com>
2022-01-02 18:37:50 +0100

    Create Day4.2.py

---
+++ Day4.2.py
@@ -0,0 +1,12 @@
+import hashlib
+Key = input("Input Key: ")
+b = 0
+while True:
+    P_Key = Key + str(b)
+    Code = hashlib.md5(P_Key.encode())
+    H_code = Code.hexdigest()
+    if H_code.startswith("000000"):
+        print(Key + str(b))
+        break
+    b += 1
+    P_Key = ""
